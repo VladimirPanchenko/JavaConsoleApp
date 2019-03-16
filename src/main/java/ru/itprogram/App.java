1a1f40567e5a7c755d1b403e8059738c6628fb8b
Pululun <kapcap4@gmail.com>
2019-03-16 23:49:12 +0400

    Added App class and created an instance of the class NoteBookProvider, the methods of the class are called NoteBookProvider

--- src/main/java/ru/itprogram/App.java
+++ src/main/java/ru/itprogram/App.java
@@ -8,8 +8,11 @@ public class App
     public static void main( String[] args ) {
         NoteBookProvider noteBookProvider = NoteBookProvider.getInstance();
 
-        NoteBookConsoleView.print(noteBookProvider.getOneRandomInstanceNote());
+        System.out.println("Вывод случайного экземпляра класса NoteBook:");
         NoteBookConsoleView.print(noteBookProvider.getOneRandomInstanceNoteBook());
+        System.out.println("Вывод случайного экземпляра класса Note из случайного NoteBook:");
+        NoteBookConsoleView.print(noteBookProvider.getOneRandomInstanceNote());
+        System.out.println("Вывод массива NoteNook:");
         NoteBookConsoleView.print(noteBookProvider.getNoteBook());
     }
 }
